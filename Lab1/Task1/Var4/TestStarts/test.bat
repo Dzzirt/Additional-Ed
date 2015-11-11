eddda0a363ee8d496cedb8ea60c27822297a502d
Nikita Kuzin <nikitakyza@gmail.com>
2015-11-11 17:22:50 +0300

    Lab1 Task1

---
+++ Lab1/Task1/Var4/TestStarts/test.bat
@@ -0,0 +1,43 @@
+cls
+@cd %~dp0
+@echo 12312312345.txt out.txt 1231234 cool
+@echo.
+@..\Debug\Var4.exe 12312312345.txt out.txt 1231234 cool
+@echo.
+@echo.
+@echo mama.txt out.txt ma mama
+@echo.
+@..\Debug\Var4.exe mama.txt out.txt ma mama
+@echo.
+@echo.
+@echo text.txt out.txt быстро 123
+@echo.
+@..\Debug\Var4.exe text.txt out.txt быстро 123
+@echo.
+@echo.
+@fsutil file createnew big.txt 3147483648
+@echo.
+@echo big.txt out.txt 1 2
+@echo.
+@..\Debug\Var4.exe big.txt out.txt 1 2
+@echo.
+@echo.
+@echo 12312312345.txt big.txt 1 2
+@echo.
+@..\Debug\Var4.exe 12312312345.txt big.txt 1 2
+del big.txt
+@echo.
+@echo.
+@echo empty.txt out.txt 1 2
+@echo.
+@..\Debug\Var4.exe empty.txt out.txt 1 2
+@echo.
+@echo.
+@echo none.txt out.txt 1 2
+@echo.
+@..\Debug\Var4.exe none.txt out.txt 1 2
+@echo.
+@echo.
+@echo none parameters
+@echo.
+@..\Debug\Var4.exe
---
+++ Lab1/Task1/Var4/Var4/main.cpp
@@ -0,0 +1,76 @@
+// Var4.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <string>
+#include <fstream>
+#include <iostream>
+#include <boost/filesystem.hpp>
+
+using namespace std;
+using namespace boost::filesystem;
+
+const uintmax_t MAX_SIZE = 2147483648;
+
+// replace.exe <input file> <output file> <search string> <replace string>
+
+void ReplaceStringInPlace(string& subject, const string& search, const string& replace)
+{
+	size_t pos = 0;
+	while ((pos = subject.find(search, pos)) != string::npos)
+	{
+		subject.replace(pos, search.length(), replace);
+		pos += replace.length();
+	}
+}
+
+int main(int argc, char* argv[])
+{
+	setlocale(LC_ALL, "rus");
+	if (argc < 5)
+	{
+		cout << "Not enought parameters to execute program!" << endl;
+	}
+	else
+	{
+		string ifile = argv[1];
+		string ofile = argv[2];
+		string search = argv[3];
+		ifstream in;
+		ofstream out;
+		in.open(ifile);
+		if (!in.is_open())
+		{
+			cout << "Input file does not exist!" << endl;
+		}
+		else if (file_size(ifile) == 0)
+		{
+			cout << "Input file is empty!" << endl;
+		}
+		else if (file_size(ifile) > MAX_SIZE)
+		{
+			cout << "Size of input file more than 2GB" << endl;
+		}
+		else if (file_size(ofile) > MAX_SIZE)
+		{
+			cout << "Size of output file more than 2GB" << endl;
+		}
+		else
+		{
+			out.open(ofile);
+			string replace = argv[4];
+			while (!in.eof())
+			{
+				string current;
+				getline(in, current);
+				ReplaceStringInPlace(current, search, replace);
+				out.write(current.c_str(), current.length());
+				cout << "Done: " + current << endl;
+			}
+			in.close();
+			out.close();
+		}
+	}
+
+	return 0;
+}
