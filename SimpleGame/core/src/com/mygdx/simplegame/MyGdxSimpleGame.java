3d33eed7c69e385d6c17588fec4c2bf853bbc7bd
Nikita Kuzin <nikitakyza@gmail.com>
2016-02-24 07:30:07 +0300

    Simple Game in dev

---
+++ SimpleGame/core/src/com/mygdx/simplegame/MyGdxSimpleGame.java
@@ -0,0 +1,59 @@
+package com.mygdx.simplegame;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class MyGdxSimpleGame extends Game {
+	SpriteBatch batch;
+	Texture img;
+
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		img = new Texture("badlogic.jpg");
+	}
+
+	@Override
+	public void pause() {
+		super.pause();
+	}
+
+	@Override
+	public void resume() {
+		super.resume();
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		super.resize(width, height);
+	}
+
+	@Override
+	public void setScreen(Screen screen) {
+		super.setScreen(screen);
+	}
+
+	@Override
+	public Screen getScreen() {
+		return super.getScreen();
+	}
+
+	@Override
+	public void render () {
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		batch.begin();
+		batch.draw(img, 0, 0);
+		batch.end();
+	}
+
+	@Override
+	public void dispose() {
+		super.dispose();
+	}
+}
---
+++ SimpleGame/desktop/src/com/mygdx/simplegame/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mygdx.simplegame.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.simplegame.MyGdxSimpleGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxSimpleGame(), config);
+	}
+}
